10befde2a1590e3fb2b69e06c58495d995ad7113
kesrishubham2510 <shubhamkeshari90@gmail.com>
2025-09-23 10:43:39 +0530

    Improved component logic

--- src/molecules/loginForm/LoginForm.jsx
+++ src/molecules/loginForm/LoginForm.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 
 import './loginForm.css';
+
+import endpoints from '../../config/API';
+
 import DataHelper from "../../utilities/DataValidator";
 
 import InputBox from "../../atoms/inputBox/InputBox";
 import Button from "../../atoms/button/Button";
 
 
-function LoginForm(props){
+function LoginForm(props) {
 
-    const [userCredentials, setUserCredentials] = useState({
+    const initialUserCredentials = {
         'email': '',
         'password': ''
-    });
+    }
 
-    const [errors, setErrors] = useState({
+    const inititalErrors = {
         'emailErr': '',
         'passwordErr': ''
-    })
+    }
 
-     const registerButtonStyle = {
+    const [userCredentials, setUserCredentials] = useState(initialUserCredentials);
+
+    const [errors, setErrors] = useState(inititalErrors)
+
+    const registerButtonStyle = {
         background: "whitesmoke",
         width: "44%",
         borderRadius: "10px",
@@ -34,39 +41,128 @@ function LoginForm(props){
         fontWeight: "bold"
     }
 
-    function updateInput(event){
-        const {name, value} = event.target;
+    function updateInput(event) {
+        const { name, value } = event.target;
+        const errkey = name + 'Err';
 
-        setUserCredentials((prevCredentials)=>{
+        setUserCredentials((prevCredentials) => {
             return {
                 ...prevCredentials,
-                [name] : value
+                [name]: value
             }
-        })
-    }
+        });
 
-    function login(event){
-        event.preventDefault();
-        console.log("User being logged in");
-        validateCredentials();
-    }
-
-    function validateCredentials(){
-        const emailErr = DataHelper.validateEmail(userCredentials.email);
-        const passwordErr = DataHelper.validatePassword(userCredentials.password, undefined);
-
-        setErrors((prevErros)=>{
+        // to nullify the errors related to the rececntly updated field
+        setErrors((prevErrors) => {
             return {
-                ...prevErros,
-                'emailErr': emailErr,
-                'passwordErr': passwordErr
+                ...prevErrors,
+                [errkey]: ''
             }
-        })
+        });
+    }
+
+    function login(event) {
+        event.preventDefault();
+
+        if (!areCredentialsValid()) {
+            return;
+        }
+
+        console.log("User being logged in");
+
+        const payload = {
+            "username": userCredentials.email,
+            "password": userCredentials.password
+        }
+
+        fetch(endpoints.login_endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }).then((receivedRresponse) => {
+            if (receivedRresponse.ok) {
+                console.log('Response status:- ', receivedRresponse.status);
+                // reset any error, 
+                setErrors(() => {
+                    return inititalErrors;
+                })
+                // empty the form fields
+                setUserCredentials(() => {
+                    return initialUserCredentials;
+                })
+                // navigate to user account
+            } else if (receivedRresponse.status === 400 || receivedRresponse.status === 401) {
+                console.log('Response status:- ', receivedRresponse.status);
+                // show the field error
+                receivedRresponse.json().then(response => handleException(response));
+            } else {
+                console.log('Do something for this issue');
+                receivedRresponse.json().then(response => {
+                    console.log(response);
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }
+
+    function areCredentialsValid() {
+
+        let emailFieldValidation = ''
+
+        if (userCredentials.email.includes('@'))
+            emailFieldValidation = DataHelper.validateEmail(userCredentials.email);
+        else
+            emailFieldValidation = DataHelper.validateUsername(userCredentials.email);
+
+        let passwordFieldValidation = DataHelper.validatePassword(userCredentials.password);
+
+        const errObj = {
+            'emailErr': emailFieldValidation,
+            'passwordErr': passwordFieldValidation
+        }
+
+        const isIdentityFieldError = !DataHelper.isEmptyOrNull(emailFieldValidation);
+        const isPasswordFieldError = !DataHelper.isEmptyOrNull(passwordFieldValidation);
+
+        setErrors(errObj);
+
+        let isValid = true;
+
+        if (isIdentityFieldError || isPasswordFieldError) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    function handleException(exceptionMessageBody) {
+        const key = exceptionMessageBody.key;
+
+        if (key === 'INVALID_IDENTITY') {
+            setErrors((prevErrors) => {
+                return {
+                    ...prevErrors,
+                    'emailErr': 'User not found'
+                }
+            })
+        } else if (key === 'WRONG_CREDENTIAL') {
+            setErrors((prevErrors) => {
+                return {
+                    ...prevErrors,
+                    'passwordErr': 'Wrong password'
+                }
+            })
+        }
+
     }
 
     return <div className="loginForm">
         <h2 className='page-label'>Login</h2>
-        <form onSubmit={login}>
+        <form>
             <InputBox id={"email"} name="email" type="text" value={userCredentials.email} onChange={updateInput} label={"Identity"} placeHolder={"email or username"} error={errors.emailErr} ></InputBox>
             <br />
             <InputBox id={"password"} name="password" type="password" value={userCredentials.password} onChange={updateInput} label={"Password"} placeHolder={"********"} error={errors.passwordErr}></InputBox>
--- src/molecules/signupForm/SignupForm.jsx
+++ src/molecules/signupForm/SignupForm.jsx
@@ -60,14 +60,14 @@ function SignupForm(props) {
             setErrors(() => {
                 return {
                     ...initialError,
-                    'emailErr': 'Email is already registered'
+                    'emailErr': 'Email is already taken'
                 }
             });
         } else if (errorKey === 'USERNAME_ALREADY_TAKEN') {
             setErrors(() => {
                 return {
                     ...initialError,
-                    'usernameErr': 'Username is already registered'
+                    'usernameErr': 'Username is already taken'
                 }
             });
         } else {
@@ -86,64 +86,57 @@ function SignupForm(props) {
 
         event.preventDefault();
 
-        const firstNameError = (DataHelper.validateFirstName(userData.firstName) === null) && errors.firstNameErr === '';
-        const lastNameError = (DataHelper.validateLastName(userData.lastName) === null) && errors.lastNameErr === '';
-        const emailError = (DataHelper.validateEmail(userData.email) === null) && errors.emailErr === '';
-        const usernameError = (DataHelper.validateUsername(userData.username) === null) && errors.usernameErr === '';
-        const passwordError = (DataHelper.validatePassword(userData.password, userData.confirmPassword) === null) && errors.passwordErr === '';
+        const isValid = isInformationValid();
 
+        if (!isValid)
+            return;
 
-        if (!(firstNameError === false && lastNameError === false && emailError === false && usernameError === false && passwordError === false)) {
-            console.log("hitting API:- " + endpointMap.registration_endpoint + " to register my user");
+        console.log("hitting API:- " + endpointMap.registration_endpoint + " to register my user");
 
-            setErrors(() => { initialError });
-
-
-            // prepare the payload for POST request
-            const payload = {
-                "firstName": userData.firstName,
-                "lastName": userData.lastName,
-                "username": userData.username,
-                "email": userData.email,
-                "password": userData.password
-            }
-            const registrationData = fetch(
-                endpointMap.registration_endpoint,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(payload)
-                }
-            ).then(response => {
-
-                if (response.ok) {
-                    response.json().then(errResponse => {
-                        console.log("Status is :- ", response.status, " response:- ", errResponse);
-                        // clear the previously seen errors and entered userData
-                        handleException(errResponse);
-                        setUserData(() => {
-                            return initialUserData
-                        })
-
-                        // navigate to user-profile page
-                    })
-                } else if (response.status === 400) {
-                    response.json().then(errResponse => {
-                        console.log("Status is :- ", response.status, " response:- ", errResponse);
-                        handleException(errResponse);
-                    })
-                } else {
-                    response.json().then(errResponse => {
-                        console.log("Status is :- ", response.status, " response:- ", errResponse);
-                        // Display a Modal to mention 'Something went wrong'
-                    })
-                }
-            }).catch(err => {
-                console.log("This is the err:- " + err);
-            })
+        // prepare the payload for POST request
+        const payload = {
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "username": userData.username,
+            "email": userData.email,
+            "password": userData.password
         }
+        const registrationData = fetch(
+            endpointMap.registration_endpoint,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            }
+        ).then(response => {
+
+            if (response.ok) {
+                response.json().then(errResponse => {
+                    console.log("Status is :- ", response.status, " response:- ", errResponse);
+                    // clear the previously entered userData
+                    // setUserData(() => {
+                    //     return initialUserData
+                    // })
+
+                    // navigate to user-profile page
+                })
+            } else if (response.status === 400) {
+                response.json().then(errResponse => {
+                    console.log("Status is :- ", response.status, " response:- ", errResponse);
+                    handleException(errResponse);
+                })
+            } else {
+                response.json().then(errResponse => {
+                    console.log("Status is :- ", response.status, " response:- ", errResponse);
+                    // Display a Modal to mention 'Something went wrong'
+                })
+            }
+        }).catch(err => {
+            console.log("This is the err:- " + err);
+        })
+
 
     }
 
@@ -169,20 +162,37 @@ function SignupForm(props) {
                 // to avoid multiple words in first name
                 [name]: value.trim()
             }
-
-            if (errors[errorKey] === '') {
-            } else {
-                removeInputError(name);
-            }
-
             return updatedState;
         });
 
+        removeInputError(errorKey);
+    }
+
+    function isInformationValid() {
+
+        const firstNameValidationResult = DataHelper.validateFirstName(userData.firstName);
+        const lastNameValidationResult = DataHelper.validateLastName(userData.lastName);
+        const emailValidationResult = DataHelper.validateEmail(userData.email);
+        const usernameValidationResult = DataHelper.validateUsername(userData.username);
+        const passwordValidationResult = DataHelper.validatePassword(userData.password, userData.confirmPassword);
+
+        const errorObject = {
+            'firstNameErr': firstNameValidationResult,
+            'lastNameErr': lastNameValidationResult,
+            'usernameErr': usernameValidationResult,
+            'emailErr': emailValidationResult,
+            'passwordErr': passwordValidationResult,
+            'confirmPasswordErr': ''
+        }
+        
+        setErrors(errorObject);
+
+        return Object.values(errorObject).every(err => !err);;
     }
 
     return <div className="signupForm">
         <h2 className='page-label'>Registration</h2>
-        <form onSubmit={register}>
+        <form>
             <InputBox id={"firstName"} name="firstName" type="text" value={userData.firstName} onChange={updateInput} label={"First name"} placeHolder="John" error={errors.firstNameErr} ></InputBox>
             <br />
             <InputBox id={"lastName"} name="lastName" type="text" value={userData.lastName} onChange={updateInput} label={"Last name"} placeHolder="Doe" error={errors.lastNameErr} ></InputBox>
